603da9987959c74f196fea76d233a9a44bb7c30a
Robin Fellinger <rfellinger.mmt-m2020@fh-salzburg.ac.at>
2021-12-01 17:57:43 +0100

    day2: part 2

--- lib/day1.ex
+++ lib/day1.ex
@@ -1,5 +1,11 @@
 defmodule Day1 do
   def main do
+    IO.puts("Part 1: " <> Integer.to_string(part1))
+    IO.puts("Part 2: " <> Integer.to_string(part2))
+
+  end
+
+  def part1 do
     Aoc21.getInput("01") |> Aoc21.getInputList() |> Aoc21.listToIntList() |> Day1.getIncreased()
   end
 
@@ -12,6 +18,28 @@ defmodule Day1 do
           %{last: e, increase: acc.increase}
         end
       end)
+
     map.increase
+  end
+
+  def part2 do
+    Aoc21.getInput("01")
+    |> Aoc21.getInputList()
+    |> Aoc21.listToIntList()
+    |> Day1.getIncreased2()
+  end
+
+  def getIncreased2(inputlist, increase \\ 0)
+
+  def getIncreased2([a, b, c, d | tail], increase) when a + b + c >= b + c + d do
+    getIncreased2([b, c, d | tail], increase)
+  end
+
+  def getIncreased2([a, b, c, d | tail], increase) when a + b + c < b + c + d do
+    getIncreased2([b, c, d | tail], increase + 1)
+  end
+
+  def getIncreased2(_, increase) do
+    increase
   end
 end
