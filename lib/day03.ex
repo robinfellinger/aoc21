71744d6657ee3d061a2c378ea953e4e1bfcc5e2e
Robin Fellinger <rfellinger.mmt-m2020@fh-salzburg.ac.at>
2021-12-03 10:59:13 +0100

    🐪 -> 🐍

--- lib/day03.ex
+++ lib/day03.ex
@@ -5,10 +5,10 @@ defmodule Day3 do
   end
 
   def part1 do
-    Aoc21.get_input("03") |> Aoc21.get_input_list() |> Aoc21.string_list_to_atoms() |> getPowerUsage()
+    Aoc21.get_input("03") |> Aoc21.get_input_list() |> Aoc21.string_list_to_atoms() |> get_power_usage()
   end
 
-  defp getBinaryCount(input, pos) do
+  defp get_binary_count(input, pos) do
     Enum.reduce(input, %{zero: 0, one: 0}, fn e, acc ->
       if Enum.at(e, pos) === "0" do
         %{zero: acc.zero + 1, one: acc.one}
@@ -18,20 +18,20 @@ defmodule Day3 do
     end)
   end
 
-  defp getPowerUsage(input, pos \\ 0, g \\ "", e \\ "")
+  defp get_power_usage(input, pos \\ 0, g \\ "", e \\ "")
 
-  defp getPowerUsage(input, pos, g, e) do
+  defp get_power_usage(input, pos, g, e) do
     if Enum.at(input, 0) |> Enum.count() === pos do
       {g, _} = Integer.parse(g, 2)
       {e, _} = Integer.parse(e, 2)
       g * e
     else
-      count = getBinaryCount(input, pos)
+      count = get_binary_count(input, pos)
 
       if count.one > count.zero do
-        getPowerUsage(input, pos + 1, g <> "1", e <> "0")
+        get_power_usage(input, pos + 1, g <> "1", e <> "0")
       else
-        getPowerUsage(input, pos + 1, g <> "0", e <> "1")
+        get_power_usage(input, pos + 1, g <> "0", e <> "1")
       end
     end
   end
@@ -41,28 +41,28 @@ defmodule Day3 do
   end
 
   defp getLifeSupport(input) do
-    gtRating(input) * gtRating(input, true)
+    get_rating(input) * get_rating(input, true)
   end
 
-  defp gtRating(input, inverted \\ false, pos \\ 0)
+  defp get_rating(input, inverted \\ false, pos \\ 0)
 
-  defp gtRating(input, inverted, pos) when pos <= 11 do
-    count = getBinaryCount(input, pos)
+  defp get_rating(input, inverted, pos) when pos <= 11 do
+    count = get_binary_count(input, pos)
 
     is_one = if inverted, do: count.one < count.zero, else: count.one >= count.zero
 
-    input = trimInput(input, pos, is_one)
+    input = trim_input(input, pos, is_one)
 
     if input |> Enum.count() === 1 do
       [head | _] = input
       {result, _} = Integer.parse(Enum.join(head), 2)
       result
     else
-      gtRating(input, inverted, pos + 1)
+      get_rating(input, inverted, pos + 1)
     end
   end
 
-  defp trimInput(input, pos, one) do
+  defp trim_input(input, pos, one) do
     number = if one, do: "1", else: "0"
     for n <- input, Enum.at(n, pos) == number, do: n
   end
